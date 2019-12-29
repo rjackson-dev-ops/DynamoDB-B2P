a4cc12900f578cd5e15805a8e8987f54f54bb78a
Robert Jackson <robert.jackson@stelligent.com>
2019-12-29 07:03:56 -0600

    got table create via cli working

--- 02-working-with-tables/load_weather_table.sh
+++ 02-working-with-tables/load_weather_table.sh
@@ -1,1 +1,6 @@
-aws dynamodb create-table --cli-input-json file://weatherstationtable.json
+#!/bin/bash
+
+# cat weatherstationtable.json  | jq '.Resources.table.Properties' > weatherstationtable_props.json
+aws dynamodb create-table --cli-input-json file://weatherstationtable_props.json
+
+# aws dynamodb describe-table --table-name=weatherstation_data
